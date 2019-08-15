5914fb7a5468360dee0edbedef4bb1eeaad75d14
dipesh <dipshrestha@gmail.com>
2019-08-15 00:32:16 -0700

    slove using the best method

--- daily/add2ToN.js
+++ daily/add2ToN.js
@@ -9,3 +9,32 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?
 */
 
+class Add2toN {
+
+	// Time: O(N^2)
+	// Space: O(1)
+	iterative(WM, W) {
+		for (let i = 0; i < WM.length; i++) {
+			for (let j = i + 1; j < WM.length; j++) {
+				if (WM[i] + WM[j] == W) return true;
+			}
+		}
+		return false;
+	}
+
+	// https://leetcode.com/problems/two-sum/solution/
+	// Use a hashmap
+	// Time: O(N)
+	// Space: O(N)
+	iterativeBest(WM, W) {
+		let m = new Set();
+		//WM.foreach(e => m.add(e)); --> no need to do this initially
+		for (let i = 0; i < WM.length; i++) {
+			if (m.has(W - WM[i])) return true;
+			m.add(WM[i]);
+		}
+		return false;
+	}
+}
+
+module.exports = Add2toN;
--- test/add2ToNTest.js
+++ test/add2ToNTest.js
@@ -7,13 +7,33 @@ beforeEach('Setting up...', function() {
     algo = new Add2ToN();
 });
 
-describe.only('Add2ToN(17)', function() {
+describe('Add2ToN(17): positive', function() {
     var WM = [10, 15, 3, 7];
     var W = 17;
     const expected = true;
 
     it('Iterative', function() {
         const result = algo.iterative(WM, W)
+        assert.equal(result, expected);
+    });
+        it('IterativeBest', function() {
+        const result = algo.iterativeBest(WM, W)
+        assert.equal(result, expected);
+    });
+});
+
+describe('Add2ToN(17): negative', function() {
+    var WM = [10, 15, 3, 7];
+    var W = 16;
+    const expected = false;
+
+    it('Iterative', function() {
+        const result = algo.iterative(WM, W)
+        assert.equal(result, expected);
+    });
+
+            it('IterativeBest', function() {
+        const result = algo.iterativeBest(WM, W)
         assert.equal(result, expected);
     });
 });
