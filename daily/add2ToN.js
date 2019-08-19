caa8e3476e158e40a6a2f9d5090444f36ed867b7
dipesh <dipshrestha@gmail.com>
2019-08-18 22:39:51 -0700

    adding difficulty level

--- daily/add2ToN.js
+++ daily/add2ToN.js
@@ -1,7 +1,10 @@
  /**
  * Created by dipshrestha on 08/14/2019.
  */
+ 
 /*
+Daily Coding Problem: Problem #1 [Easy]
+
 Good morning! Here's your coding interview problem for today.
 This problem was recently asked by Google.
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
