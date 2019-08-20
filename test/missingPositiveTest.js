25d875e9aa4ef32b506699e245de7dd45cac0b7c
Dipesh Shrestha <dipshrestha@gmail.com>
2019-08-20 15:18:01 -0700

    Daily Coding Challenge Day 4

---
+++ test/missingPositiveTest.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const MissingPositive = require('../daily/MissingPositive.js');
+
+let algo = null;
+
+beforeEach('Setting up...', function() {
+    algo = new MissingPositive();
+});
+
+describe('MissingPositive - 1', function() {
+    var WM = [3, 4, -1, 1];
+    const expected = 2;
+
+    it('Iterative', function() {
+        const result = algo.iterative(WM)
+        assert.equal(result, expected);
+    });
+
+});
+//[ should give 3.
+
+describe('MissingPositive - 2', function() {
+    var WM = [1, 2, 0];
+    const expected = 3;
+
+    it('Iterative', function() {
+        const result = algo.iterative(WM)
+        assert.equal(result, expected);
+    });
+
+});
