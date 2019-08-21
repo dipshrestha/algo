a1f38c8de3d39099a5342f2cb03accc72751b174
Dipesh Shrestha <dipshrestha@gmail.com>
2019-08-21 16:23:50 -0700

    add template scaffolding

---
+++ daily/template.js
@@ -0,0 +1,17 @@
+/**
+ * Created by dipshrestha on mm/dd/yyyy
+ */
+
+/*
+Daily Coding Problem: Problem # [Difficulty]
+
+[Desc]
+
+ */
+
+class ClassName {
+
+
+}
+
+module.exports = ClassName;
---
+++ test/templateTest.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Template', function() {
+  const result = 3;
+  const expected = 3;
+
+  it('Iterative', function() {
+    assert.equal(result, expected);
+  });
+});
