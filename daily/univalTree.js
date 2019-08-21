8baca5b2fdcd9a17ecea0b8ab445b4ed4f9ca6c1
Dipesh Shrestha <dipshrestha@gmail.com>
2019-08-21 13:26:19 -0700

    Daily Coding Problem: Problem #8

---
+++ daily/univalTree.js
@@ -0,0 +1,49 @@
+/**
+ * Created by dipshrestha on 08/21/2019.
+ */
+
+/*
+Daily Coding Problem: Problem #8 [Easy]
+
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Google.
+A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+Given the root to a binary tree, count the number of unival subtrees.
+For example, the following tree has 5 unival subtrees:
+
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+
+*/
+
+const { Node } = require('../daily/serializeTree.js');
+
+class UnivalTree {
+  constructor() {
+    this._init();
+  }
+
+  _init() {
+    this.counter = 0;
+  }
+  // time:  O(|WM| * N)
+  // space: O(|N|)
+  recursive(node) {
+    this._recursive_helper(node);
+    return this.counter;
+  }
+
+  _recursive_helper(node) {
+    if (node == null) return null;
+    if (this._recursive_helper(node.left) == this._recursive_helper(node.right))
+      this.counter++;
+    return node.val;
+  }
+}
+
+module.exports = UnivalTree;
---
+++ test/univalTreeTest.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const UnivalTree = require('../daily/univalTree.js');
+const { Node } = require('../daily/serializeTree.js');
+
+let algo = null;
+
+beforeEach('Setting up...', function() {
+  algo = new UnivalTree();
+});
+/*
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+*/
+describe(' UnivalTree - 1', function() {
+  var node = new Node('0', new Node('1'), new Node('0', new Node('1', new Node('1'), new Node('1')), new Node('0')));
+
+  it('Recursive', function() {
+    const result = algo.recursive(node);
+    const expected = 5;
+    assert.equal(result, expected);
+  });
+});
+
+
+/*
+   0
+  / \
+ 1   0
+    /  \
+   1    0
+  / \. / \
+ 1   1 0  0
+*/
+describe(' UnivalTree - 2', function() {
+  var node = new Node('0', 
+    new Node('1'), new Node('0', 
+                new Node('1', 
+            new Node('1'), 
+        new Node('1')), 
+                            new Node('0', 
+                        new Node('0'), new Node('0'))));
+
+  it('Recursive', function() {
+    const result = algo.recursive(node);
+    const expected = 7;
+    assert.equal(result, expected);
+  });
+});
