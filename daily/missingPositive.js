be374a2bc9c234fc8669807713d33508b0face8e
dipesh <dipshrestha@gmail.com>
2019-08-21 00:24:52 -0700

    adding best method

--- daily/missingPositive.js
+++ daily/missingPositive.js
@@ -33,33 +33,50 @@
 
    // Time: O(N)
    // Space: O(1)
-   iterativeBest1(WM) {
-     // fill elements <=0 with null
-     console.log(WM);
+   iterativeBest(WM) {
+     // move negative numbers to the end and find the end index of +ve numbers
+     var good = -1;
      for (let i = 0; i < WM.length; i++) {
-       if (WM[i] <= 0) WM[i] = null;
+       if (WM[i] > 0) {
+         if (good != i - 1) {
+           this._swap(WM, good + 1, i)
+           good += 1;
+         } else {
+           good = i;
+         }
+       }
      }
-     console.log(WM);
-     for (let i = 0; i < WM.length; i++) {
+     //console.log(WM);
+     //console.log('good: ', good + 1);
+
+     // mark existing element by making it's index negative
+     for (let i = 0; i < good + 1; i++) {
+       let x = Math.abs(WM[i]);
+       if (x <= good + 1) {
+         WM[x - 1] = -1 * Math.abs(WM[x - 1]);
+       }
+     }
+     //console.log(WM);
+
+     // the solution is the first index with +ve value
+     for (let i = 0; i < good + i; i++) {
        let x = WM[i];
-       if (x < WM.length) WM[Math.abs(x)] *= -1;
-       //if (x < WM.length) WM[x] *= -1;
+       if (x >= 0) return i + 1;
      }
-     console.log(WM);
-     for (let i = 1; i < WM.length; i++) {
-       let x = WM[i];
-       if (x == null) return i;
-     }
-     return WM.length;
+     return good;
    }
+
    _swap(nums, i, j) {
      var temp = nums[i];
      nums[i] = nums[j];
      nums[j] = temp;
    }
+
    // Time: O(N)
    // Space: O(1)
-   iterativeBest(nums) {
+   // This is a very good way
+   // swap until nums[i] != i+1 or nums[i] is out of range( >1 or < N)
+   iterativeBest_copied1(nums) {
      let n = nums.length;
      for (let i = 0; i < nums.length; i++) {
        if (nums[i] >= 1 && nums[i] <= n) {
@@ -80,6 +97,8 @@
    }
 
    //https://leetcode.com/problems/first-missing-positive/discuss/361903/48-ms-faster-than-96.85-of-JavaScript-online-submissions
+   // Time: O(N)
+   // Space: O(1)
    iterativeBest_copied(nums) {
      const len = nums.length;
      for (let i = 0; i < len; i++) {
