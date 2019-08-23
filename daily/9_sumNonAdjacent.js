cbb67e139b22da3faea0c3e45316f22509bf7616
Dipesh Shrestha <dipshrestha@gmail.com>
2019-08-23 12:28:28 -0700

    Recursive

--- daily/9_sumNonAdjacent.js
+++ daily/9_sumNonAdjacent.js
@@ -14,9 +14,22 @@ Follow-up: Can you do this in O(N) time and constant space?
  */
 
 class SumNonAdjacent {
+  //  [2, 4, 6, 2, 5]
 
-  recursive() {
+  //. F(0) = F(0)     = 0
+  //. F(1) = F(2)     = 2
+  //. F(2) = F(24)    =    Max(F(0) + 4, F(1)) = 4
+  //. F(3) = F(246)   =    Max(F(1) + 6, F(2) - 2 + 2) = 8
+  //. F(4) = F(2462)  =    Max(F(2) + 2, F(3) - 6 + 2) = 8
+  //. F(5) = F(24625) =    Max(F(3) + 5, F(4) - 2 + 5) = 13
 
+  recursive(WM) {
+    if (WM.length == 0) return 0;
+    if (WM.length == 1) return WM[0];
+
+    var p0 = this.recursive(WM.slice(0, WM.length - 2)) + WM[WM.length - 1];
+    var p1 = this.recursive(WM.slice(0, WM.length - 1)) - WM[WM.length - 2] + WM[WM.length - 1];
+    return Math.max(p0, p1);
   }
 
   iterative() {
--- test/9_sumNonAdjacentTest.js
+++ test/9_sumNonAdjacentTest.js
@@ -7,17 +7,32 @@ beforeEach('Setting up...', function() {
   algo = new SumNonAdjacent();
 });
 
-describe.skip('SumNonAdjacent -1', function() {
+
+describe('SumNonAdjacent [2, 4]', function() {
+  var WM = [2, 4];
+  const expected = 4;
+
+  it('Recursive', function() {
+    const result = algo.recursive(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe('SumNonAdjacent [2, 4, 6, 2, 5]', function() {
   var WM = [2, 4, 6, 2, 5];
   const expected = 13;
 
-  it('Iterative', function() {
-    const result = algo.iterative(WM)
+  it('Recursive', function() {
+    const result = algo.recursive(WM)
     assert.equal(result, expected);
   });
-
+});
+describe('SumNonAdjacent  [5, 1, 1, 5]', function() {
+  var WM =  [5, 1, 1, 5];
+  const expected = 10;
+  
   it('Recursive', function() {
-    const result = algo.iterativeBest(WM)
+    const result = algo.recursive(WM)
     assert.equal(result, expected);
   });
 });
