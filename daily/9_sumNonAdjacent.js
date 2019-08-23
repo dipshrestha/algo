7353cf17311171775a9b89cd5017343ad02058c4
Dipesh Shrestha <dipshrestha@gmail.com>
2019-08-23 14:41:10 -0700

    add iterative method

--- daily/9_sumNonAdjacent.js
+++ daily/9_sumNonAdjacent.js
@@ -13,6 +13,9 @@ Follow-up: Can you do this in O(N) time and constant space?
 
  */
 
+/**
+ This is like Fibonacci
+ */
 class SumNonAdjacent {
   //  [2, 4, 6, 2, 5]
   //. F(0) = F(0)     = 0
@@ -32,22 +35,40 @@ class SumNonAdjacent {
   //. F(4) = F(2462)  =    Max(F(2) + 2, F(3) - 6 + 2) = 8
   //. F(5) = F(24625) =    Max(F(3) + 5, F(4) - 2 + 5) = 13
   // ====================================================== //
+
+
+  // Time:  O(2^N)
+  // Space: O(N)
   recursive(WM) {
     if (WM.length == 0) return 0;
     if (WM.length == 1) return WM[0];
     // no harm including this line
     //if (WM.length == 2) return Math.max(WM[0], WM[1]);
 
-    var p0 = this.recursive(WM.slice(0, WM.length - 2)) + WM[WM.length - 1];
-    var p1 = this.recursive(WM.slice(0, WM.length - 1));
+    var p1 = this.recursive(WM.slice(0, WM.length - 2)) + WM[WM.length - 1];
+    var p2 = this.recursive(WM.slice(0, WM.length - 1));
 
     // Previously.....thought this way....
     //var p1 = this.recursive(WM.slice(0, WM.length - 1)) - WM[WM.length - 2] + WM[WM.length - 1];
-    return Math.max(p0, p1);
+    return Math.max(p1, p2);
   }
 
-  iterative() {
 
+  // Time:  O(N)
+  // Space: O(1)
+  // NOTE: loop doesn't run through end of the WM.length like i<=WM.length;
+  iterative(WM) {
+    if (WM.length == 0) return 0;
+    if (WM.length == 1) return WM[0];
+    var p1 = WM[0],
+      p2 = WM[1],
+      f = Math.max(p1, p2);
+    for (var i = 2; i < WM.length; i++) {
+      p2 = p1 + WM[i];
+      p1 = f;
+      f = Math.max(p1, p2);
+    }
+    return f;
   }
 
   dp() {
--- test/9_sumNonAdjacentTest.js
+++ test/9_sumNonAdjacentTest.js
@@ -16,6 +16,11 @@ describe('SumNonAdjacent [2, 4]', function() {
     const result = algo.recursive(WM)
     assert.equal(result, expected);
   });
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
 });
 
 describe('SumNonAdjacent [4, 2]', function() {
@@ -24,6 +29,11 @@ describe('SumNonAdjacent [4, 2]', function() {
 
   it('Recursive', function() {
     const result = algo.recursive(WM)
+    assert.equal(result, expected);
+  });
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
@@ -36,34 +46,54 @@ describe('SumNonAdjacent [2, 4, 6, 2, 5]', function() {
     const result = algo.recursive(WM)
     assert.equal(result, expected);
   });
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
 });
 
 describe('SumNonAdjacent  [-1,-5]', function() {
-  var WM =  [-1, -5];
+  var WM = [-1, -5];
   const expected = -1;
-  
+
   it('Recursive', function() {
     const result = algo.recursive(WM)
+    assert.equal(result, expected);
+  });
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
 
 describe('SumNonAdjacent  [-5,-1]', function() {
-  var WM =  [-5, -1];
+  var WM = [-5, -1];
   const expected = -1;
-  
+
   it('Recursive', function() {
     const result = algo.recursive(WM)
+    assert.equal(result, expected);
+  });
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
 
 describe('SumNonAdjacent  [5, 4, -1, 3, 6, -4]', function() {
-  var WM =  [5, 4, -1, 3, 6, -4];
+  var WM = [5, 4, -1, 3, 6, -4];
   const expected = 11;
-  
+
   it('Recursive', function() {
     const result = algo.recursive(WM)
+    assert.equal(result, expected);
+  });
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
