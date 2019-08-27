5e619b295e8e20ecc20ded5d52b95a28c0bf1bfd
Dipesh Shrestha <dipshrestha@gmail.com>
2019-08-26 17:19:28 -0700

    -recursive

--- daily/11_autocomplete.js
+++ daily/11_autocomplete.js
@@ -13,66 +13,86 @@ Hint: Try preprocessing the dictionary into a more efficient data structure to s
 
  */
 
+
+/*
+
+ each level is a pointer to a new hash map
+ [abc, abcd, abd, adb]
+  a -> b -> c -> [-1,-1]
+              -> d -> [-1,-1]
+          > d
+       d -> b
+
+*/
 class Autocomplete {
 
   constructor(WM) {
     this._init(WM);
   }
 
-  _addWord(parentMap, word) {
-    if (word == null || word == '') return -1;
-    if (word.length == 1) {
-      var childMap = parentMap.get(word[i]);
-      if (childMap) {
-        return childMap;
-      }
-      childMap = new Map();
-      childMap.set(word[i], childMap);
-      return childMap;
-    } else {
-      var lastChar = word.substr(-1);
-      var childMap = this._addWord(parentMap.get(), word.substr(-1));
-      parentMap.set()
+  _addWord(map, word) {
+    if (word == null || word == '') {
+      map.set(-1, -1);
+      return;
+      //return map;
     }
+    var char = word.charAt(0); // get first char
+    var childMap = map.get(char);
+    if (!childMap) {
+      childMap = new Map();
+    }
+    this._addWord(childMap, word.substr(1));
+    map.set(char, childMap);
   }
 
   _init(WM) {
-
+    this.map = new Map();
     // for each word
     //   for each letter find hashMap of the letter and add it to appropriate place
     var parentMap = new Map();
     for (var i = 0; i < WM.length; i++) {
       var word = WM[i];
-      /*
-      for (var j = 0; j < word.length; i++) {
-        var letter = word[i];
-        var childMap = parentMap.has(letter)
-        if (childMap) {
-          childMap.push(letter, letter);
-
-        }
-        childMap = new Map();
-        childMap.push(letter, new Map())
-      }*/
+      this._addWord(this.map, word);
     }
   }
 
+  /**
+  Traverse till the end you reach -1: return the path
+   **/
+  _getAll(map) {
+    if (map == null || map.size == 0) return null;
+    // for each of the entries in map.
+    var ret = [];
+    for (var [key, value] of map.entries()) {
+      if (value == -1) {
+        ret.push('');
+        continue;
+      }
+      var x = this._getAll(value);
 
-  _getAll() {
-
-  }
-
-  recursive(maps, searchString) {
-    // find inside the child
-    while (searchString != null) {
-      var s = searchString[i];
-      if (maps.has(s)) {
-        maps = maps.get(s);
+      for (var i = 0; i < x.length; i++) {
+        var y = x[i];
+        ret.push(key + y);
       }
     }
-    return _getAll(maps);
+    return ret;
   }
 
+  // time: O(N)
+  // space: O(N) -- stack call
+  recursive(searchString) {
+    var s = this._getAll(this._recursive_helper(this.map, searchString));
+    return s.map(f => searchString + f);
+  }
+
+  _recursive_helper(map, searchString) {
+    if (map == null || map.size == 0) return null;
+    if (searchString == null || searchString == '') return map;
+    var char = searchString.charAt(0); // get first char
+    var childMap = map.get(char);
+    var res = this._recursive_helper(childMap, searchString.substr(1)); // excluding the first one
+    return res;
+  }
 }
 
 module.exports = Autocomplete;
--- test/11_autocompleteTest.js
+++ test/11_autocompleteTest.js
@@ -4,18 +4,50 @@ const Autocomplete = require('../daily/11_autocomplete.js');
 let algo = null;
 
 beforeEach('Setting up...', function() {
-  const WM = ['dog', 'deer', 'deal'];
+  const WM = ['dog', 'deer', 'deal', 'acid', 'acids', 'bags', 'bag'];
   algo = new Autocomplete(WM);
 });
 
-describe('Template', function() {
+describe('Autocomplete - 1', function() {
   const searchString = 'de';
   const expected = ['deer', 'deal'];
 
   it('Recursive', function() {
-    var result = ['deer', 'deal'];
-    //algo.recursive(searchString)
-    //const result = algo.recursive(searchString)
+    var result = algo.recursive(searchString);
+    console.log(result);
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('Autocomplete - single letter', function() {
+  const searchString = 'd';
+  const expected = ['dog', 'deer', 'deal'];
+
+  it('Recursive', function() {
+    var result = algo.recursive(searchString);
+    console.log(result);
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('Autocomplete - single letter ', function() {
+  const searchString = 'a';
+  const expected = ['acid', 'acids'];
+
+  it('Recursive', function() {
+    var result = algo.recursive(searchString);
+    console.log(result);
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('Autocomplete - single letter ', function() {
+  const searchString = 'b';
+  const expected = ['bags', 'bag'];
+
+  it('Recursive', function() {
+    var result = algo.recursive(searchString);
+    console.log(result);
     assert.deepEqual(result, expected);
   });
 });
