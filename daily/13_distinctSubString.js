03a0dc0c0213e795e8e3f9048211015d8622f6c1
dipesh <dipshrestha@gmail.com>
2019-08-28 00:49:28 -0700

    WIP recursive method

--- daily/13_distinctSubString.js
+++ daily/13_distinctSubString.js
@@ -13,7 +13,35 @@ For example, given s = "abcba" and k = 2, the longest substring with k distinct 
  */
 
 class DistinctSubString {
+  constructor() {
+    this._init();
+  }
 
+  _init() {
+    this.maxLength = 0;
+  }
+
+  recursive(str, N) {
+    var ret = this.recursive_helper(str, N);
+    return Math.max(this.maxLength, ret.length);
+  }
+  recursive_helper(str, N) {
+    debugger
+    if (str == null || str.length == 0) return '';
+    //if (N == 0) return '';
+    if (str.length == 1 && N >= 1) return str;
+
+    //for (var i = 1; i <= N; i++) {
+    var e = str.substr(-1); // get the last element
+    //var remaining = 123;
+    var ret = this.recursive_helper(str.substr(0, str.length - 1), N - 1)
+    if (ret.indexOf(e) != -1 || ret.length < N) {
+      return ret + e;
+    } else {
+      this.maxLength = Math.max(this.maxLength, ret.length);
+      return e;
+    }
+  }
 
 }
 
--- test/13_distinctSubString.js
+++ test/13_distinctSubString.js
@@ -7,11 +7,77 @@ beforeEach('Setting up...', function() {
   algo = new DistinctSubString();
 });
 
-describe('DistinctSubString', function() {
-  const result = 3;
+describe('DistinctSubString(a,1)', function() {
+  const str = 'a';
+  const N = 1;
+  const expected = 1;
+
+  it('Recursive', function() {
+    const result = algo.recursive(str, N);
+    assert.equal(result, expected);
+  });
+});
+describe('DistinctSubString(ab,1)', function() {
+  const str = 'ab';
+  const N = 1;
+  const expected = 1;
+
+  it('Recursive', function() {
+    const result = algo.recursive(str, N);
+    assert.equal(result, expected);
+  });
+});
+describe('DistinctSubString(aa,1)', function() {
+  const str = 'ab';
+  const N = 1;
+  const expected = 1;
+
+  it('Recursive', function() {
+    const result = algo.recursive(str, N);
+    assert.equal(result, expected);
+  });
+});
+
+describe('DistinctSubString(ab,2)', function() {
+  const str = 'ab';
+  const N = 2;
+  const expected = 2;
+
+  it('Recursive', function() {
+    const result = algo.recursive(str, N);
+    assert.equal(result, expected);
+  });
+});
+
+describe('DistinctSubString(abc,2)', function() {
+  const str = 'abc';
+  const N = 2;
+  const expected = 2;
+
+  it('Recursive', function() {
+    const result = algo.recursive(str, N);
+    assert.equal(result, expected);
+  });
+});
+
+describe.skip('DistinctSubString(bba,2)', function() {
+  const str = 'bba';
+  const N = 2;
   const expected = 3;
 
-  it('Iterative', function() {
+  it('Recursive', function() {
+    const result = algo.recursive(str, N);
+    assert.equal(result, expected);
+  });
+});
+
+describe.skip('DistinctSubString(aba,2)', function() {
+  const str = 'aba';
+  const N = 2;
+  const expected = 3;
+
+  it('Recursive', function() {
+    const result = algo.recursive(str, N);
     assert.equal(result, expected);
   });
 });
