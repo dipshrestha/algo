bf1f6edfbe54b518532e72bebd04633346a80d98
Dipesh Shrestha <dipshrestha@gmail.com>
2019-08-29 14:15:05 -0700

    more tests

--- daily/16_recordLog.js
+++ daily/16_recordLog.js
@@ -7,16 +7,49 @@ Daily Coding Problem: Problem #16 [Easy]
 
 Good morning! Here's your coding interview problem for today.
 This problem was asked by Twitter.
-You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish this, with the following API:
+You run an e-commerce website and want to record the last N order ids in a log. 
+Implement a data structure to accomplish this, with the following API:
 record(order_id): adds the order_id to the log
 get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N.
 You should be as efficient with time and space as possible.
 
  */
 
-class RecordLog {
-
-
+class LinkListNode {
+  constructor(val, before = null) {
+    this.before = before;
+    this.val = val;
+  }
 }
 
-module.exports = RecordLog;
+class RecordLog {
+
+  constructor() {
+    this.head = null;
+  }
+
+  record(order_id) {
+    var x = null;
+    if (this.head == null) {
+      x = new LinkListNode(order_id, null);
+    } else {
+      x = new LinkListNode(order_id, this.head);
+    }
+    this.head = x;
+  }
+
+
+  // time: O(k)
+  // space: O(N)
+  get_last(i) {
+    if (i <= 0) return null;
+    var tmp = this.head;
+    while (i - 1 > 0 && tmp != null) {
+      tmp = tmp.before;
+      i--;
+    }
+    return tmp ? tmp.val : null;
+  }
+}
+
+module.exports = { RecordLog, LinkListNode }
--- test/16_recordLogTest.js
+++ test/16_recordLogTest.js
@@ -1,17 +1,40 @@
 const assert = require('assert');
-const RecordLog = require('../daily/16_recordLog.js');
+const { RecordLog, LinkListNode } = require('../daily/16_recordLog.js');
 
 let algo = null;
 
 beforeEach('Setting up...', function() {
   algo = new RecordLog();
+  algo.record(1);
+  algo.record(2);
+  algo.record(3);
 });
 
-describe('Template', function() {
-  const result = 3;
-  const expected = 3;
+describe('RecordLog 123', function() {
+  it('pointer movement 0', function() {
+    const expected = null;
+    const result = algo.get_last(0);
+    assert.equal(result, expected);
+  })
+  it('pointer movement 1', function() {
+    const expected = 3;
+    const result = algo.get_last(1);
+    assert.equal(result, expected);
+  })
 
-  it('Iterative', function() {
+  it('pointer movement 2', function() {
+    const expected = 2;
+    const result = algo.get_last(2);
     assert.equal(result, expected);
   });
+  it('pointer movement 3', function() {
+    const expected = 1;
+    const result = algo.get_last(3);
+    assert.equal(result, expected);
+  });
+  it('pointer movement 4', function() {
+    const expected = null;
+    const result = algo.get_last(4);
+    assert.equal(result, expected);
+  });;
 });
