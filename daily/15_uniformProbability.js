6451b0f0ca3c726bb178f56b0c0136eaf0aa7b41
dipesh <dipshrestha@gmail.com>
2019-08-30 00:01:27 -0700

    refactor

--- daily/15_uniformProbability.js
+++ daily/15_uniformProbability.js
@@ -13,18 +13,34 @@ Given a stream of elements too large to store in memory, pick a random element f
 
 // https://www.youtube.com/watch?v=Ybra0uGEkpM - very good explanation
 // https://www.youtube.com/watch?v=8-YLI2hRfpo
+
+/*
+https://www.geeksforgeeks.org/select-a-random-number-from-stream-with-o1-space/
+
+How does this work
+We need to prove that every element is picked with 1/n probability where n is the number of items seen so far. 
+For every new stream item x, we pick a random number from 0 to ‘count -1’, if the picked number is ‘count-1’, 
+we replace the previous result with x.
+*/
 class UniformProbability {
 
   random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  // Time: O(N)
+  // Space: O(1)
   generate(stream) {
     var rand = stream[0],
       cur = null;
 
     for (var i = 1; i < stream.length; i++) {
-      if (this.random(0, i) == i) rand = cur;
+      cur = stream[i]
+      // get random from 1 to i
+      // a) compare to certain value;
+      if (this.random(1, i + 1) == i) rand = cur;
+      // b) OR below this
+      //if (this.random(1, i + 1) == 1) rand = cur;
     }
     return rand;
   }
