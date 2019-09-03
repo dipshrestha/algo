ed7e34a34e6c7b6ce59295ff977d1182ec0de1bf
dipesh <dipshrestha@gmail.com>
2019-09-03 00:36:31 -0700

    change template

--- test/templateTest.js
+++ test/templateTest.js
@@ -1,10 +1,16 @@
 const assert = require('assert');
+//const LongestAbsolutePath = require('../daily/17_longestAbsolutePath.js');
 
+let algo = null;
+
+beforeEach('Setting up...', function() {
+  //algo = new LongestAbsolutePath();
+});
 describe('Template', function() {
-  const result = 3;
   const expected = 3;
 
   it('Iterative', function() {
+    const result = 3;//algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
