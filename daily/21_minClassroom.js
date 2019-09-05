25575b2e415135aaba8b3951647199426ee1ef56
Dipesh Shrestha <dipshrestha@gmail.com>
2019-09-05 15:00:43 -0700

    Add iterative

--- daily/21_minClassroom.js
+++ daily/21_minClassroom.js
@@ -15,6 +15,25 @@ For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 
 class MinClassroom {
 
+  iterative(WM) {
+    // sort first
+    WM.sort((a, b) => a[0] > b[0]);
+    console.log(WM);
+
+    var conflict = 1,
+      rooms = 1,
+      baseLine = WM[0][1];
+    for (var i = 0; i < WM.length - 1; i++) {
+      if (baseLine >= WM[i + 1][0]) {
+        conflict++;
+      } else {
+        baseLine = WM[i + 1][1];
+        rooms = Math.max(conflict, rooms);
+        conflict = 0;
+      }
+    }
+    return rooms;
+  }
 
 }
 
--- test/21_minClassroomTest.js
+++ test/21_minClassroomTest.js
@@ -7,11 +7,46 @@ beforeEach('Setting up...', function() {
   algo = new MinClassroomTest();
 });
 
-describe('Template', function() {
-  const expected = 3;
+describe('MinClassroomTest - 1', function() {
+  const expected = 2;
+  const WM = [
+    [30, 75],
+    [0, 50],
+    [60, 150]
+  ];
 
   it('Iterative', function() {
-    const result = 3; //algo.iterative(WM)
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe('MinClassroomTest - 2', function() {
+  const expected = 3;
+  const WM = [
+    [30, 75],
+    [0, 50],
+    [31, 55],
+    [60, 150]
+  ];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe('MinClassroomTest - 3', function() {
+  const expected = 1;
+  const WM = [
+    [30, 35],
+    [0, 15],
+    [20, 25],
+    [60, 150]
+  ];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
