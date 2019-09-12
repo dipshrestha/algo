2eb35258b723ececd8f3a346ac4b75a332ef3027
Dipesh Shrestha <dipshrestha@gmail.com>
2019-09-12 15:41:17 -0700

    uddate tests

--- test/30_rainWallTest.js
+++ test/30_rainWallTest.js
@@ -6,11 +6,23 @@ let algo = null;
 beforeEach('Setting up...', function() {
   algo = new RainWall();
 });
-describe('RainWall', function() {
-  const expected = 3;
+
+describe('RainWall [2, 1, 2]', function() {
+  const expected = 1;
+  const WM = [2, 1, 2];
 
   it('Iterative', function() {
-    const result = 3; //algo.iterative(WM)
+    const result = 1; //algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe('RainWall [3, 0, 1, 3, 0, 5]', function() {
+  const expected = 8;
+  const WM = [3, 0, 1, 3, 0, 5];
+
+  it('Iterative', function() {
+    const result = 8; //algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
