b4b8b625cc7e128486b584d2a3fd5b7d7c80694a
dipesh <dipshrestha@gmail.com>
2019-09-17 23:11:01 -0700

    tests

--- test/31_editDistanceTest.js
+++ test/31_editDistanceTest.js
@@ -6,11 +6,47 @@ let algo = null;
 beforeEach('Setting up...', function() {
   algo = new EditDistance();
 });
-describe('Template', function() {
+describe('EditDistance: ab,b', function() {
+  const expected = 1;
+
+  it('Recursive', function() {
+    const str1 = "ab",
+      str2 = "b";
+    const result = algo.recursive(str1, str2)
+    assert.equal(result, expected);
+  });
+});
+
+describe('EditDistance: b, ab', function() {
+  const expected = 1;
+
+  it('Recursive', function() {
+    const str1 = "ab",
+      str2 = "b";
+    const result = algo.recursive(str2, str1);
+    assert.equal(result, expected);
+  });
+});
+
+describe('EditDistance: ab,bc', function() {
+  const expected = 2;
+
+  it('Recursive', function() {
+    const str1 = "ab",
+      str2 = "bc";
+    const result = algo.recursive(str1, str2)
+    assert.equal(result, expected);
+  });
+});
+
+
+describe.only('EditDistance: kitten, sitting', function() {
   const expected = 3;
 
-  it('Iterative', function() {
-    const result = 3; //algo.iterative(WM)
+  it('Recursive', function() {
+    const str1 = "kitten",
+      str2 = "sitting";
+    const result = algo.recursive(str1, str2)
     assert.equal(result, expected);
   });
 });
