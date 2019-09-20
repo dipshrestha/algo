1bcabb0159d68d444f6d65486048d2bcc4a840dd
dipesh <dipshrestha@gmail.com>
2019-09-20 08:08:35 -0700

    wip

--- daily/34_editDistancePalindrome.js
+++ daily/34_editDistancePalindrome.js
@@ -19,6 +19,22 @@ As another example, given the string "google", you should return "elgoogle".
 
 class EditDistancePalindrome {
 
+  _getLeft(str) {
+    var left = this.recursive(str.slice(1)); // exclude first
+    left = left.map(i => str.slice(0, 1) + i + str.slice(0, 1));
+  }
+  _getRight(str) {
+    var right = this.recursive(str.slice(0, -1)); // exclude last
+    right = right.map(i => str.slice(-1) + str.charAt(0) + str.slice(-1));
+  }
+  recursive(str) {
+    if (str.length == 0 || str == "") return [];
+    if (str.length == 1) return [str];
+    if (str.length == 2) {
+      if (str.charAt(0) == str.charAt(1)) return [str];
+
+    }
+  }
 
 }
 
