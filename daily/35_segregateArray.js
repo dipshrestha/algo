7ef7ea30d7ef83371030d64bca5a19e556957f64
Dipesh Shrestha <dipshrestha@gmail.com>
2019-09-23 16:57:14 -0700

    WIP

--- daily/35_segregateArray.js
+++ daily/35_segregateArray.js
@@ -7,14 +7,60 @@ Daily Coding Problem: Problem #35 [Hard]
 
 Good morning! Here's your coding interview problem for today.
 This problem was asked by Google.
-Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
+Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that 
+all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
 Do this in linear time and in-place.
-For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become 
+['R', 'R', 'R', 'G', 'G', 'B', 'B'].
 
  */
 
 class SegregateArray {
+  constructor(orderArr) {
+    this.V = orderArr;
+  }
 
+  _swap(WM, i, j) {
+    var tmp = WM[i];
+    WM[i] = WM[j];
+    WM[j] = tmp;
+  }
+
+  _isGreater(WM, i, j) {
+    return this.V.indexOf(WM[i]) > this.V.indexOf(WM[j])
+  }
+
+  // place pointer in both end, left for smallest, right for largest
+  // move pointer inward only if 
+  // - left is
+  // - right is
+  iterative(WM) {
+    var L = 0,
+      R = WM.length - 1;
+
+    while (L < R) {
+      //if (WM[L] > WM[R]) {
+      if (this._isGreater(WM, L, R)) {
+        this._swap(WM, R, L);
+        //if (WM[L] == this.V[0])
+        //  L++;
+        //} else if (WM[L] < WM[R]) {
+      } else {
+        this._swap(WM, L, R);
+        //if (WM[R] == this.V[this.V.length - 1])
+        //  R--;
+      }
+      if (WM[L] == this.V[0])
+        L++;
+      if (WM[R] == this.V[this.V.length - 1])
+        R--;
+      if (WM[L] == WM[R] && WM[L] == this.V[1]) {
+        L++;
+        //R--;
+      }
+    }
+    return WM;
+  }
 
 }
 
--- test/35_segregateArrayTest.js
+++ test/35_segregateArrayTest.js
@@ -4,13 +4,136 @@ const SegregateArray = require('../daily/35_segregateArray.js');
 let algo = null;
 
 beforeEach('Setting up...', function() {
-  algo = new SegregateArray();
+  algo = new SegregateArray(['R', 'G', 'B']);
 });
-describe('Template', function() {
-  const expected = 3;
+
+describe.skip('SegregateArray - GBRRBRG ', function() {
+  const expected = ['R', 'R', 'R', 'G', 'G', 'B', 'B'];
+  const WM = ['G', 'B', 'R', 'R', 'B', 'R', 'G'];
 
   it('Iterative', function() {
-    const result = 3;//algo.iterative(WM)
-    assert.equal(result, expected);
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+
+describe.skip('SegregateArray - GRGB ', function() {
+  const expected = ['R', 'G', 'G', 'B'];
+  const WM = ['G', 'R', 'G', 'B'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - BRGB ', function() {
+  const expected = ['R', 'G', 'B', 'B'];
+  const WM = ['B', 'R', 'G', 'B'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - RGR ', function() {
+  const expected = ['R', 'R', 'G'];
+  const WM = ['R', 'G', 'R'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - GBG ', function() {
+  const expected = ['G', 'G', 'B'];
+  const WM = ['G', 'B', 'G'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe.skip('SegregateArray - GRG ', function() {
+  const expected = ['R', 'G', 'G'];
+  const WM = ['G', 'R', 'G'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - BRB ', function() {
+  const expected = ['R', 'B', 'B'];
+  const WM = ['B', 'R', 'B'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - BRG ', function() {
+  const expected = ['R', 'G', 'B'];
+  const WM = ['B', 'R', 'G'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - BRR ', function() {
+  const expected = ['R', 'R', 'B'];
+  const WM = ['B', 'R', 'R'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - RG ', function() {
+  const expected = ['R', 'G'];
+  const WM = ['R', 'G'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - GR', function() {
+  const expected = ['R', 'G'];
+  const WM = ['G', 'R'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - GB ', function() {
+  const expected = ['G', 'B'];
+  const WM = ['G', 'B'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
+  });
+});
+
+describe('SegregateArray - BG ', function() {
+  const expected = ['G', 'B'];
+  const WM = ['B', 'G'];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.deepEqual(result, expected);
   });
 });
