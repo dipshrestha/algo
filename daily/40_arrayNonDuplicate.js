736775a879f42fc3407a27e23b603b6eeda41962
dipesh <dipshrestha@gmail.com>
2019-09-28 00:29:16 -0700

    iterative

--- daily/40_arrayNonDuplicate.js
+++ daily/40_arrayNonDuplicate.js
@@ -13,8 +13,37 @@ Do this in O(N) time and O(1) space.
 
  */
 
+/*
+ Algo : https://www.geeksforgeeks.org/find-the-element-that-appears-once/
+ We can sum the bits in same positions for all the numbers and take modulo with 3. The bits for which sum is not multiple of 3, are the bits of number with single occurrence.
+Let us consider the example array {5, 5, 5, 8}. The 101, 101, 101, 1000
+Sum of first bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+Sum of second bits%3 = (0 + 0 + 0 + 0)%0 = 0;
+Sum of third bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+Sum of fourth bits%3 = (1)%3 = 1;
+Hence number which appears once is 1000
+*/
 class ArrayNonDuplicate {
 
+  // Time: O(N)
+  // Space: O(1)
+  iterative(WM) {
+    var x, sum, result = 0;
+    for (var i = 0; i < 32; i++) {
+      sum = 0;
+      x = (1 << i);
+      for (var j = 0; j < WM.length; j++) {
+        if ((WM[j] & x) == 0) sum++;
+      }
+      //console.log(result);
+      if ((sum % 3) == 0) {
+        console.log(result, x);
+        result |= x;
+      }
+      //console.log(result);
+    }
+    return result;
+  }
 
 }
 
--- test/40_arrayNonDuplicateTest.js
+++ test/40_arrayNonDuplicateTest.js
@@ -7,11 +7,42 @@ beforeEach('Setting up...', function() {
   algo = new ArrayNonDuplicate();
 });
 
-describe('ArrayNonDuplicate', function() {
-  const expected = 3;
+describe('ArrayNonDuplicate: 2,2,2,4', function() {
+  const expected = 4;
+  const WM = [2,2,2,4]
 
   it('Iterative', function() {
-    const result = 3;//algo.iterative(WM)
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe('ArrayNonDuplicate: 2,4,2,2', function() {
+  const expected = 4;
+  const WM = [2,4,2,2]
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe('ArrayNonDuplicate: [13, 19, 13, 13]', function() {
+  const expected = 19;
+  const WM = [13, 19, 13, 13]
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe('ArrayNonDuplicate: [6, 1, 3, 3, 3, 6, 6]', function() {
+  const expected = 1;
+  const WM = [6, 1, 3, 3, 3, 6, 6]
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
