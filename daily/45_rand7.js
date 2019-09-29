289ad4b720ecadcabb30160c7c1504705e91de12
dipesh <dipshrestha@gmail.com>
2019-09-29 10:50:17 -0700

    add recursive

--- daily/45_rand7.js
+++ daily/45_rand7.js
@@ -11,8 +11,29 @@ Using a function rand5() that returns an integer from 1 to 5 (inclusive) with un
 implement a function rand7() that returns an integer from 1 to 7 (inclusive).
  */
 
+/*
+ Algo : https://www.geeksforgeeks.org/generate-integer-from-1-to-7-with-equal-probability/
+ and why simply we can't use
+_last = foo();
+
+rand7()
+{
+_last = (_last + foo()) % 7 + 1;
+return _last;
+}
+*/
 class Rand7 {
 
+  rand5() {
+    return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+  }
+
+  rand7() {
+    var tmp = 5 * this.rand5() + this.rand5() - 5;
+    if (tmp < 22)
+      return tmp % 7 + 1;
+    return this.rand7();
+  }
 
 }
 
--- test/45_rand7Test.js
+++ test/45_rand7Test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const Rand7 = require('../daily/45_rand7.js');
+const { expect } = require('chai')
 
 let algo = null;
 
@@ -7,11 +8,15 @@ beforeEach('Setting up...', function() {
   algo = new Rand7();
 });
 
-describe('Rand7', function() {
-  const expected = 3;
+describe('Rand7: only test number between 1-7. doesn not test randomness', function() {
+  const expected = 7;
 
   it('Iterative', function() {
-    const result = 3; //algo.iterative(WM)
-    assert.equal(result, expected);
+    for (var i = 0; i < 50; i++) {
+      var result = algo.rand7();
+      console.log(result);
+      expect(result).to.be.gt(0); //(result, expected);
+      expect(result).to.be.lt(expected + 1); //(result, expected);
+    }
   });
 });
