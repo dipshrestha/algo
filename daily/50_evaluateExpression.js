b4cc2550c1ceb699d891c891b8635adf3177b796
Dipesh Shrestha <dipshrestha@gmail.com>
2019-10-03 10:15:03 -0700

    add recursive

--- daily/50_evaluateExpression.js
+++ daily/50_evaluateExpression.js
@@ -7,7 +7,8 @@ Daily Coding Problem: Problem #50 [Easy]
 
 Good morning! Here's your coding interview problem for today.
 This problem was asked by Microsoft.
-Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+Suppose an arithmetic expression is given as a binary tree. 
+Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
 Given the root to such a tree, write a function to evaluate it.
 For example, given the following tree:
 
@@ -20,13 +21,40 @@ You should return 45, as it is (3 + 2) * (4 + 5).
 
  */
 
+const { Node } = require('../daily/3_serializeTree.js');
 /*
- Algo: Solve using backtracking
+ Algo: traverse node in post order
 */
 class EvaluateExpression {
 
-  // Time: O(2^N)
+  _isLeaf(node) {
+    return node.left == null && node.right == null;
+  }
+  // Time: O(N)
   // Space: O(N)
+  recursive(node) {
+    debugger;
+    if (node == null) return 0;
+    var val = this._recursive_helper(node);
+    return val;
+  }
+
+  _recursive_helper(node) {
+    if (this._isLeaf(node)) return +node.val;
+    var left = this._recursive_helper(node.left);
+    var right = this._recursive_helper(node.right);
+    switch (node.val) {
+      case '+':
+        return left + right;
+      case '-':
+        return left - right;
+      case '*':
+        return left * right;
+      case '/':
+        return left / right;
+    }
+    return node.val;
+  }
 
 }
 
--- test/50_evaluateExpressionTest.js
+++ test/50_evaluateExpressionTest.js
@@ -1,16 +1,57 @@
 const assert = require('assert');
 const EvaluateExpression = require('../daily/50_evaluateExpression.js');
+const { Node } = require('../daily/3_serializeTree.js');
 
 let algo = null;
 
 beforeEach('Setting up...', function() {
   algo = new EvaluateExpression();
 });
-describe('EvaluateExpression', function() {
-  const expected = 3;
 
-  it('Iterative', function() {
-    const result = 3;//algo.iterative(WM)
+/*
+    *
+   / \
+  +    +
+ / \  / \
+3  2  4  5
+*/
+describe(' EvaluateExpression - 1', function() {
+  var node = new Node('*', new Node('+', new Node('3'), new Node('2')), new Node('+', new Node('4'), new Node('5')));
+
+  it('Recursive', function() {
+    const result = algo.recursive(node);
+    const expected = 45;
+    assert.equal(result, expected);
+  });
+});
+
+
+/*
+    *
+   / \
+  6   +
+     / \
+     4  5
+*/
+describe(' EvaluateExpression - 2', function() {
+  var node = new Node('*', new Node('6'), new Node('+', new Node('4'), new Node('5')));
+
+  it('Recursive', function() {
+    const result = algo.recursive(node);
+    const expected = 54;
+    assert.equal(result, expected);
+  });
+});
+
+/*
+    8
+*/
+describe(' EvaluateExpression - 3', function() {
+  var node = new Node('8');
+
+  it('Recursive', function() {
+    const result = algo.recursive(node);
+    const expected = 8;
     assert.equal(result, expected);
   });
 });
