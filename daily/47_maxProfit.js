c7b8e58fe8b37e6b2e82e63f5132b3f6ff50dde2
Dipesh Shrestha <dipshrestha@gmail.com>
2019-10-14 16:14:40 -0700

    add iterative

--- daily/47_maxProfit.js
+++ daily/47_maxProfit.js
@@ -8,13 +8,41 @@ Daily Coding Problem: Problem #47 [Easy]
 Good morning! Here's your coding interview problem for today.
 This problem was asked by Facebook.
 Given a array of numbers representing the stock prices of a company in chronological order, 
-write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
-For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
+write a function that calculates the maximum profit you could have made from buying and 
+selling that stock once. You must buy before you can sell it.
+For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could 
+buy the stock at 5 dollars and sell it at 10 dollars.
 
  */
 
+
+/*
+ Algo: First pass, start from right and find max at each point and store.
+ Second pass, start from left and find max difference between each point and the max.
+*/
 class MaxProfit {
 
+  iterative(WM) {
+    if (WM == null || WM.length < 2) return null;
+
+    var mem = [],
+      max = WM[WM.length - 1],
+      profit = 0;
+
+    for (var i = WM.length - 2; i >= 0; i--) {
+      if (WM[i] > max) {
+        max = WM[i];
+      }
+      mem[i + 1] = max;
+    }
+
+    console.log(mem);
+    for (var i = 0; i < WM.length - 1; i++) {
+      if (mem[i + 1] - WM[i] > profit)
+        profit = mem[i + 1] - WM[i];
+    }
+    return profit;
+  }
 
 }
 
--- test/47_maxProfitTest.js
+++ test/47_maxProfitTest.js
@@ -7,11 +7,32 @@ beforeEach('Setting up...', function() {
   algo = new MaxProfit();
 });
 
-describe('MaxProfit', function() {
-  const expected = 3;
+describe.only('MaxProfit', function() {
+  const expected = 5;
+  const WM = [9, 11, 8, 5, 7, 10];
 
   it('Iterative', function() {
-    const result = 3;//algo.iterative(WM)
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe.only('MaxProfit', function() {
+  const expected = 8;
+  const WM = [12, 2, 4, 10];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
+
+describe.only('MaxProfit', function() {
+  const expected = 0;
+  const WM = [12, 10, 9];
+
+  it('Iterative', function() {
+    const result = algo.iterative(WM)
     assert.equal(result, expected);
   });
 });
