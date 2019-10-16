8e82fe5e161761e573d605d11aa2df91848eaeb7
Dipesh Shrestha <dipshrestha@gmail.com>
2019-10-16 15:56:48 -0700

    Daily Coding Problem: Problem #46 [Hard]

---
+++ daily/46_longestPalindrome.js
@@ -0,0 +1,27 @@
+/**
+ * Created by dipshrestha on 10/16/2019
+ */
+
+/*
+Daily Coding Problem: Problem #46 [Hard]
+
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Amazon.
+Given a string, find the longest palindromic contiguous substring. 
+If there are more than one with the maximum length, return any one.
+For example, the longest palindromic substring of "aabcdcb" is "bcdcb". T
+he longest palindromic substring of "bananas" is "anana".
+
+ */
+
+/*
+ Algo: Solve using backtracking
+*/
+class LongestPalindrome {
+
+  // Time: O(2^N)
+  // Space: O(N)
+
+}
+
+module.exports = LongestPalindrome;
