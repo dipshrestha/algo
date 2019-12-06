e63f502d323e5e888703b59f58ae489f07fbc912
Dipesh Shrestha <dipshrestha@gmail.com>
2019-12-06 12:16:52 -0800

    Daily Coding Problem: Problem #58 [Medium]

---
+++ daily/58_rotateArray.js
@@ -0,0 +1,27 @@
+/**
+ * Created by dipshrestha on 12/05/2019
+ */
+
+/*
+Daily Coding Problem: Problem #58 [Medium]
+
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Amazon.
+An sorted array of integers was rotated an unknown number of times.
+Given such an array, find the index of the element in the array in faster than linear time. If the element doesn't exist in the array, return null.
+For example, given the array [13, 18, 25, 2, 8, 10] and the element 8, return 4 (the index of 8 in the array).
+You can assume all the integers in the array are unique.
+
+ */
+
+/*
+ Algo: Solve using backtracking
+*/
+class RotateArray {
+
+  // Time: O(2^N)
+  // Space: O(N)
+
+}
+
+module.exports = RotateArray;
