ce5f39e134904f7b98fcc053004870f038dbf8c3
Dipesh Shrestha <dipshrestha@gmail.com>
2020-01-06 13:47:24 -0800

    recursive method similar to backtracking

--- daily/60_partitionMultisetInteger.js
+++ daily/60_partitionMultisetInteger.js
@@ -19,9 +19,61 @@ Given the multiset {15, 5, 20, 10, 35}, it would return false, since we can't sp
 */
 class PartitionMultisetInteger {
 
-  // Time: O(2^N)
-  // Space: O(N)
 
+  recursive(WM) {
+    var sum = WM.reduce((f, s) => f + s);
+    if (sum % 2 != 0) return false;
+    var res = this._recursive_helper(WM, sum / 2);
+    return res;
+  }
+
+
+  // Time: O(N^N) Exponential
+  // Space: O(N)
+  /**
+   * check if it's possible to add elements to N
+   **/
+  _recursive_helper(WM, N, index = 0) {
+    if (N == 0) return true;
+    for (; index < WM.length; index++) {
+      var elm = WM[index];
+      if (elm == N) return true;
+      var ret = false;
+      if (elm < N)
+        ret = this._recursive_helper(WM, N - elm, index + 1);
+      if (ret)
+        return true;
+    }
+
+    return false;
+  }
+
+  /**
+  *
+  * Other method of doing the same thing
+  *
+  */
+
+  /*
+  Above is better method
+    _recursive_helper(WM, N) {
+      if (N == 0) return true;
+      //if (N < 0) return false;
+      for (var i = 0; i < WM.length; i++) {
+        var elm = WM[i];
+        if (elm == N) return true;
+        var Y = [...WM];
+        Y.splice(i, 1);
+        var ret = false;
+        if (elm < N)
+          ret = this._recursive_helper(Y, N - elm);
+        if (ret)
+          return true;
+      }
+
+      return false;
+    }
+  */
 }
 
 module.exports = PartitionMultisetInteger;
--- test/60_partitionMultisetIntegerTest.js
+++ test/60_partitionMultisetIntegerTest.js
@@ -6,11 +6,40 @@ let algo = null;
 beforeEach('Setting up...', function() {
   algo = new PartitionMultisetInteger();
 });
-describe('PartitionMultisetInteger', function() {
-  const expected = 3;
 
-  it('Iterative', function() {
-    const result = 3; //algo.iterative(WM)
-    assert.equal(result, expected);
+describe('PartitionMultisetInteger', function() {
+
+  it('Recursive: [5, 5]', function() {
+    const result = algo.recursive([5, 5])
+    assert.equal(result, true);
+  });
+
+  it('Recursive: [5]', function() {
+    const result = algo.recursive([5])
+    assert.equal(result, false);
+  });
+
+  it('Recursive: [2, 3, 5, 5, 5]', function() {
+    const result = algo.recursive([2, 3, 5, 5, 5])
+    assert.equal(result, true);
+  });
+
+  it('Recursive: [5, 5, 5, 2, 3]', function() {
+    const result = algo.recursive([5, 5, 5, 2, 3])
+    assert.equal(result, true);
+  });
+  it('Recursive: [5, 5, 5, 3, 2]', function() {
+    const result = algo.recursive([5, 5, 5, 3, 2])
+    assert.equal(result, true);
+  });
+
+  it('Recursive: [15, 5, 20, 10, 35, 1]', function() {
+    const result = algo.recursive([15, 5, 20, 10, 35, 1])
+    assert.equal(result, false);
+  });
+
+  it('Recursive: [15, 5, 20, 10, 35, 15, 10]', function() {
+    const result = algo.recursive([15, 5, 20, 10, 35, 15, 10])
+    assert.equal(result, true);
   });
 });
