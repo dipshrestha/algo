75c5d8c924b1c5033311cb6ab4a41defe7ac75ee
Dipesh Shrestha <dipshrestha@gmail.com>
2020-02-03 11:07:16 -0800

    Daily Coding Problem: Problem #64 [Hard]

---
+++ daily/64_knightTour.js
@@ -0,0 +1,25 @@
+/**
+ * Created by dipshrestha on 02/03/2020
+ */
+
+/*
+Daily Coding Problem: Problem #64 [Hard]
+
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Google.
+A knight's tour is a sequence of moves by a knight on a chessboard such that all squares are visited once.
+Given N, write a function to return the number of knight's tours on an N by N chessboard.
+
+ */
+
+/*
+ Algo: Solve using backtracking
+*/
+class KnightTour {
+
+  // Time: O(2^N)
+  // Space: O(N)
+
+}
+
+module.exports = KnightTour;
---
+++ test/64_knightTourTest.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const KnightTour = require('../daily/64_KnightTour.js');
+
+let algo = null;
+
+beforeEach('Setting up...', function() {
+  algo = new KnightTour();
+});
+
+describe('KnightTour', function() {
+  const expected = 3;
+
+  it('Iterative', function() {
+    const result = 3; //algo.iterative(WM)
+    assert.equal(result, expected);
+  });
+});
