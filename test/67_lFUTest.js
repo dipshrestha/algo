45f9fb5580673425570c846624c26f2776cca863
Dipesh Shrestha <dipshrestha@gmail.com>
2020-02-04 13:03:02 -0800

    Daily Coding Problem: Problem #67 [Hard]

---
+++ test/67_lFUTest.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const LFU = require('../daily/67_lFU.js');
+
+let algo = null;
+
+beforeEach('Setting up...', function() {
+  algo = new LFU();
+});
+
+describe.skip('LFU', function() {
+  const expected = 3;
+
+  it('Iterative', function() {
+    const result = 3;
+    algo.set('a', 1);
+    algo.set('b', 2);
+    algo.set('c', 3);
+    assert.equal('(c:3)(b:2)(a:1)', algo._getAll());
+
+    algo.set('d', 4);
+    assert.equal('(d:4)(c:3)(b:2)', algo._getAll());
+
+    algo.set('b', 12);
+    assert.equal('(b:12)(d:4)(c:3)', algo._getAll());
+
+    algo.set('e', 5);
+    assert.equal('(e:5)(b:12)(d:4)', algo._getAll());
+
+    algo.set('b', 22);
+    assert.equal('(b:22)(e:5)(d:4)', algo._getAll());
+
+    algo.set('f', 6);
+    assert.equal('(f:6)(b:22)(e:5)', algo._getAll());
+  });
+});
