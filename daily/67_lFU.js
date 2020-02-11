82a2fbf584afb53c5477143a376accda5ed413d0
Dipesh Shrestha <dipshrestha@gmail.com>
2020-02-10 17:11:31 -0800

    WIP

--- daily/67_lFU.js
+++ daily/67_lFU.js
@@ -18,29 +18,102 @@ Each operation should run in O(1) time.
  */
 
 /*
- Algo: Solve using backtracking
+ Algo: Use double linked list with 2 HashMap
+ User input <K1, V> is changed as below
+ Value hashmap <K1, Node(K1, V, next, prev, K2)>
+ Frequency hashmap <K2, (Double Linked List)>
 */
 class Node {
-  constructor(key, val, next = null, prev = null) {
+  constructor(key, val, next = null, prev = null, freq = 0) {
     this.key = key;
     this.val = val;
     this.next = next;
     this.prev = prev;
+    this.freq = freq;
+  }
+}
+
+class DLinkedList {
+  remove(node) {
+    if (!node) console.warn("Node is empty, can't be deleted")
+    // special case
+    if (node.key == tail.key) {
+      this.tail = node.prev;
+    }
+    if (node.key == head.key) {
+      this.head = node.next;
+    }
+    if (node.prev) {
+      node.prev.next = node.next;
+    }
+    if (node.next) {
+      node.next.prev = node.prev;
+    }
+    node.next = node.prev = null;
+    return node;
+  }
+
+  removeTail() {
+    if (!this.tail) console.warn("Tail is empty, can't be removed")
+    return this.remove(this.tail);
+  }
+
+  addToHead(node) {
+    if (!node) console.warn("Node is empty, can't be added");
+    if (this.head) {
+      node.next = this.head;
+      this.head.prev = node;
+    } else {
+      this.tail = node;
+    }
+    this.head = node;
+  }
+
+  constructor(head, tail) {
+    this.head = head;
+    this.tail = tail;
   }
 }
 
 class LFU {
 
   constructor(n) {
-    this.head = null;
-    this.tail = null;
+    //this.head = null;
+    //this.tail = null;
+    this.minFreq = 0;
     this.maxSize = n;
-    this.map = new Map();
+    this.vMap = new Map(n);
+    this.fMap = new Map(n);
   }
 
-  updateFrequency() {
+  _addFrequency(newNode, freq = 0) {
+    // get linked list
+    var dlink = this.fMap.get(freq);
+    if (!dlink) {
+      dlink = new fNode(newNode, newNode);
+      this.fMap.put(freq, dlink);
+    } else {
+      var head = dlink.head;
+      var tail = dlink.tail;
 
+      head.prev = newNode;
+      newNode.next = head;
+      dlink.head = newNode;
+    }
   }
+
+  _updateFrequency(existingNode) {
+    // get linked list
+    var dlink = this.fMap.get(existingNode.freq);
+    if (!dlink) {
+      console.warn("DLink is empty, _updateFrequency");
+    } else {
+      dlink.remove(existingNode);
+    }
+
+    this._addFrequency(existingNode, existingNode + 1);
+  }
+
   // Time: O(1)
   // Space: O(N)
   get(key) {
@@ -55,13 +128,29 @@ class LFU {
   }
 
   _add(key, val) {
+    if (this.vmap.size == this.maxSize) {
+      debugger;
+      //this._remove(this.tail.key);
+    }
+    var newNode = new Node(key, val);
+    this._addFrequency(newNode);
+  }
 
+  _update(key, val) {
+    var vNode = this.vmap.get(key);
+    this._updateFrequency(vNode);
   }
 
   // Time: O(1)
   // Space: O(N)
   set(key, val) {
-    var ret = this.map.get(key);
+    var obj = this.map.get(key);
+    if (!obj) {
+      this.minFreq = 1;
+      this._add(key, val);
+    } else {
+      this._update(key, val);
+    }
   }
 }
 
