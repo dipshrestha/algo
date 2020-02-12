7f3a69a8136cc64d8947089a8fb6a4a3aeafd4f9
Dipesh Shrestha <dipshrestha@gmail.com>
2020-02-11 16:05:48 -0800

    WIP

--- daily/67_lFU.js
+++ daily/67_lFU.js
@@ -22,6 +22,7 @@ Each operation should run in O(1) time.
  User input <K1, V> is changed as below
  Value hashmap <K1, Node(K1, V, next, prev, K2)>
  Frequency hashmap <K2, (Double Linked List)>
+ Frequency Double Linked List
 */
 class Node {
   constructor(key, val, next = null, prev = null, freq = 0) {
@@ -34,13 +35,24 @@ class Node {
 }
 
 class DLinkedList {
+
+  _getAll() {
+    var h = this.head;
+    var p = '';
+    while (h) {
+      p += ('(' + h.key + ':' + h.val + ')');
+      h = h.next;
+    }
+    return p;
+  }
+
   remove(node) {
     if (!node) console.warn("Node is empty, can't be deleted")
     // special case
-    if (node.key == tail.key) {
+    if (node.key == this.tail.key) {
       this.tail = node.prev;
     }
-    if (node.key == head.key) {
+    if (node.key == this.head.key) {
       this.head = node.next;
     }
     if (node.prev) {
@@ -50,101 +62,127 @@ class DLinkedList {
       node.next.prev = node.prev;
     }
     node.next = node.prev = null;
+    this.size--;
     return node;
   }
 
   removeTail() {
     if (!this.tail) console.warn("Tail is empty, can't be removed")
+    this.size--;
     return this.remove(this.tail);
   }
 
   addToHead(node) {
     if (!node) console.warn("Node is empty, can't be added");
     if (this.head) {
-      node.next = this.head;
       this.head.prev = node;
+      node.next = this.head;
     } else {
       this.tail = node;
     }
     this.head = node;
+    this.size++;
   }
 
-  constructor(head, tail) {
+  constructor(size = 0, head = null, tail = null) {
     this.head = head;
     this.tail = tail;
+    this.size = size;
   }
 }
 
 class LFU {
 
-  constructor(n) {
-    //this.head = null;
-    //this.tail = null;
-    this.minFreq = 0;
-    this.maxSize = n;
-    this.vMap = new Map(n);
-    this.fMap = new Map(n);
+  _getAll() {
+    var keys = Array.from(this.vMap.keys());
+    keys.sort();
+    var p = '';
+
+    keys.forEach(key => p += ('(' + key + ':' + this.vMap.get(key).val + ')'));
+    return p;
   }
 
-  _addFrequency(newNode, freq = 0) {
+  _remove(freq) {
+    // remove the tail
+    // get linked list
+    var dlink = this.fMap.get(freq);
+    var nodeToDelete = null;
+    if (!dlink) {
+      console.warn("DLink is empty, _remove");
+      return;
+    } else {
+      nodeToDelete = dlink.removeTail();
+      // free up hashtable
+      if (dlink.size == 0) {
+        this.fMap.delete(nodeToDelete.key)
+      }
+    }
+    this.vMap.delete(nodeToDelete.key);
+  }
+
+  constructor(n) {
+    this.minFreq = 0;
+    this.maxSize = n;
+    this.vMap = new Map();
+    this.fMap = new Map();
+  }
+
+  _addFrequency(newNode, freq = 1) {
+    newNode.freq = freq;
     // get linked list
     var dlink = this.fMap.get(freq);
     if (!dlink) {
-      dlink = new fNode(newNode, newNode);
-      this.fMap.put(freq, dlink);
+      dlink = new DLinkedList();
+      dlink.addToHead(newNode);
+      this.fMap.set(freq, dlink);
     } else {
-      var head = dlink.head;
-      var tail = dlink.tail;
-
-      head.prev = newNode;
-      newNode.next = head;
-      dlink.head = newNode;
+      dlink.addToHead(newNode);
     }
   }
 
   _updateFrequency(existingNode) {
     // get linked list
     var dlink = this.fMap.get(existingNode.freq);
+    // remove first
     if (!dlink) {
       console.warn("DLink is empty, _updateFrequency");
+      return;
     } else {
       dlink.remove(existingNode);
     }
-
-    this._addFrequency(existingNode, existingNode + 1);
-  }
-
-  // Time: O(1)
-  // Space: O(N)
-  get(key) {
-    var ret = this.map.get(key);
-    // doesn't exist
-    if (!ret)
-      return null;
-
-    updateFrequency();
-
-    return ret.val;
+    this._addFrequency(existingNode, existingNode.freq + 1);
   }
 
   _add(key, val) {
-    if (this.vmap.size == this.maxSize) {
-      debugger;
-      //this._remove(this.tail.key);
+    if (this.vMap.size == this.maxSize) {
+      this._remove(this.minFreq);
     }
     var newNode = new Node(key, val);
+    this.vMap.set(key, newNode);
     this._addFrequency(newNode);
   }
 
   _update(key, val) {
-    var vNode = this.vmap.get(key);
+    var vNode = this.vMap.get(key);
+    vNode.val = val;
     this._updateFrequency(vNode);
   }
 
   // Time: O(1)
   // Space: O(N)
+  get(key) {
+    var existingNode = this.vMap.get(key);
+    // doesn't exist
+    if (!ret)
+      return null;
+    this._updateFrequency(existingNode)
+    return ret.val;
+  }
+
+  // Time: O(1)
+  // Space: O(N)
   set(key, val) {
-    var obj = this.map.get(key);
+    var obj = this.vMap.get(key);
     if (!obj) {
       this.minFreq = 1;
       this._add(key, val);
--- test/67_lFUTest.js
+++ test/67_lFUTest.js
@@ -4,32 +4,41 @@ const LFU = require('../daily/67_lFU.js');
 let algo = null;
 
 beforeEach('Setting up...', function() {
-  algo = new LFU();
+  algo = new LFU(3);
 });
 
-describe.skip('LFU', function() {
-  const expected = 3;
+describe.only('LFU', function() {
 
   it('Iterative', function() {
     const result = 3;
     algo.set('a', 1);
     algo.set('b', 2);
     algo.set('c', 3);
-    assert.equal('(c:3)(b:2)(a:1)', algo._getAll());
+    assert.equal('(a:1)(b:2)(c:3)', algo._getAll());
 
     algo.set('d', 4);
-    assert.equal('(d:4)(c:3)(b:2)', algo._getAll());
+    assert.equal('(b:2)(c:3)(d:4)', algo._getAll());
 
     algo.set('b', 12);
-    assert.equal('(b:12)(d:4)(c:3)', algo._getAll());
+    assert.equal('(b:12)(c:3)(d:4)', algo._getAll());
 
     algo.set('e', 5);
-    assert.equal('(e:5)(b:12)(d:4)', algo._getAll());
+    assert.equal('(b:12)(d:4)(e:5)', algo._getAll());
 
     algo.set('b', 22);
-    assert.equal('(b:22)(e:5)(d:4)', algo._getAll());
+    assert.equal('(b:22)(d:4)(e:5)', algo._getAll());
 
     algo.set('f', 6);
-    assert.equal('(f:6)(b:22)(e:5)', algo._getAll());
+    assert.equal('(b:22)(e:5)(f:6)', algo._getAll());
+/*
+    algo.set('e', 6);
+    assert.equal('(b:22)(e:6)(f:6)', algo._getAll());
+
+    algo.set('f', 16);
+    assert.equal('(b:22)(e:6)(f:16)', algo._getAll());
+
+    algo.set('g', 7);
+    assert.equal('(b:22)(e:6)(g:7)', algo._getAll());
+    */
   });
 });
