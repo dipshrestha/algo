8dbf31b6c1b107a930c4d2910466dbda7ace9c8c
Dipesh Shrestha <dipshrestha@gmail.com>
2020-02-24 12:05:36 -0800

    Refactor

--- daily/75_longestIncreasingSubsequenceArray.js
+++ daily/75_longestIncreasingSubsequenceArray.js
@@ -26,9 +26,9 @@ class LongestIncreasingSubsequenceArray {
 
   // Time: O(2^N)
   // Space: O(N)
-  recursive(WM) {
+  backtracking(WM) {
     this.V = [];
-    this._recursive_helper(WM, 0, []);
+    this._backtracking_helper(WM, 0, []);
     return this.V.length;
   }
 
@@ -43,16 +43,16 @@ class LongestIncreasingSubsequenceArray {
     return pos == WM.length - 1;
   }
 
-  _recursive_helper(WM, pos, list) {
+  _backtracking_helper(WM, pos, list) {
     if (this._isFinished(WM, pos)) return list;
 
     for (var i = pos; i < WM.length; i++) {
       var o = WM[i];
       list.push(o);
       if (this._isValid(list)) {
-        this._recursive_helper(WM, i, list);
+        this._backtracking_helper(WM, i, list);
         // No need to send copy of array as below
-        //var updateList = this._recursive_helper(WM, pos + i, [...list]);
+        //var updateList = this._backtracking_helper(WM, pos + i, [...list]);
         if (this._isFinished(WM, i)) {
           if (list.length >= this.V.length) {
             this.V = [...list]; // IMP: this should be a copy
@@ -66,6 +66,16 @@ class LongestIncreasingSubsequenceArray {
     return list;
   }
 
+  /*
+  * What happens if I add myself to all other previous indices considering them as the last indices?
+  * SubProblem:
+  * F(5) = Max(F(4), F(3), F(2), F(1)) + 1 or 0
+  * F(4) = Max(F(3), F(2), F(1)) + 1 or 0
+  * F(3) = Max(F(2), F(1)) + 1 or 0
+  * 
+  * SubStucture:  F(N) = Max(F(N - 1)) + 1 if arr[N] > arr[N-1]
+                       = Max(F(N - 1)) + 0
+  */
   dp(WM) {
     if (WM == null || WM.length == 1) return 1;
     this.V = new Array(WM.length).fill(1);
@@ -79,16 +89,16 @@ class LongestIncreasingSubsequenceArray {
     return Math.max(...this.V);
   }
   /*
-  _recursive_helper1(WM, pos, list) {
+  _backtracking_helper1(WM, pos, list) {
     if (pos == WM.length - 1) return list;
 
     for (var i = pos; i < WM.length; i++) {
       var o = WM[i];
       list.push(o);
       if (this._isValid(list)) {
-        this._recursive_helper(WM, i, list);
+        this._backtracking_helper(WM, i, list);
         // No need to send cloned array as below
-        //var updateList = this._recursive_helper(WM, pos + i, [...list]);
+        //var updateList = this._backtracking_helper(WM, pos + i, [...list]);
         if (i == WM.length - 1)
           console.log(list, updateList);
       }
--- test/75_longestIncreasingSubsequenceArrayTest.js
+++ test/75_longestIncreasingSubsequenceArrayTest.js
@@ -10,8 +10,8 @@ beforeEach('Setting up...', function() {
 describe('LongestIncreasingSubsequenceArray', function() {
   const expected = 6;
   const WM = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15];
-  it('Iterative', function() {
-    const result = algo.recursive(WM)
+  it('Backtracking', function() {
+    const result = algo.backtracking(WM)
     assert.equal(result, expected);
   });
 
