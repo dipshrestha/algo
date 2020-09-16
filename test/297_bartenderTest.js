0adbe0753a718c2acd6116c198ded4e4f20e1b28
dipesh <dipshrestha@gmail.com>
2020-09-16 10:32:55 -0700

    Add new test case

--- test/297_bartenderTest.js
+++ test/297_bartenderTest.js
@@ -36,4 +36,33 @@ describe('Bartender', function() {
       assert.equal(result, expected);
     });
   })
+  describe('Highest not choosen', function() {
+    const expected = 2;
+    let preferences = [
+      [1, 4],
+      [0, 4],
+      [0, 4],
+      [0, 4],
+      [0, 3],
+      [2, 3]
+    ];
+    /*
+        int [] [] a= {{1,2,3,4},
+            {0},
+            {5},
+            {4,5},
+            {0,1,2,3}};
+    int[][] a = { { 1, 4 }, // 0
+      { 0, 4 }, // 1
+      { 0, 4 }, // 2
+      { 0, 4 }, // 3
+      { 0, 3 }, // 4
+      { 2, 3 }, // 5
+    };
+    */
+    it('Recursive', function() {
+      const result = algo.recursive(preferences)
+      assert.equal(result, expected);
+    });
+  })
 });
