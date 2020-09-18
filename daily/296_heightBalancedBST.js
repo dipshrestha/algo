5b40251108d175d6a6e46aa3c98dc950d229aef5
dipesh <dipshrestha@gmail.com>
2020-09-18 13:49:12 -0700

    * recursive

--- daily/296_heightBalancedBST.js
+++ daily/296_heightBalancedBST.js
@@ -11,14 +11,55 @@ Given a sorted array, convert it into a height-balanced binary search tree.
 
  */
 
+const { Node } = require('../daily/3_serializeTree.js');
+
 /*
- Algo: Solve using backtracking
+ Algo: 
+ Find the center
+  For each half (right/left) of center create BST
 */
-class HeightBalancedBST{
+class HeightBalancedBST {
 
-  // Time: O(2^N)
+  // Time: O(N)
   // Space: O(N)
+  recursive(N) {
+    var res = this._recursive_helper(N);
+    return res;
+  }
 
+  _getRootIndex(N) {
+    return Math.ceil((N.length + 1) / 2) - 1;
+  }
+
+  _recursive_helper(N) {
+    if (N == null || N.length < 1) return null;
+    //const h = this._getHeight(N);
+    //const r = this._getRootIndex(N, h);
+    const r = this._getRootIndex(N);
+    const left = this._recursive_helper(N.slice(0, r));
+    const right = this._recursive_helper(N.slice(r + 1, N.length));
+    const node = new Node(N[r], left, right);
+    return node;
+  }
+
+  //_getHeight(N) {
+  //  return Math.ceil(Math.log2(N.length + 1));
+  //}
+
+  _getHeight(root) {
+    if (root == null) return 0;
+    if (root.left == null && root.right == null) return 1;
+    const lHeight = this._getHeight(root.left);
+    const rHeight = this._getHeight(root.right);
+    return Math.max(lHeight, rHeight) + 1;
+  }
+
+  isBalanced(root) {
+    const lHeight = this._getHeight(root.left);
+    const rHeight = this._getHeight(root.right);
+    if (Math.abs(lHeight - rHeight) > 1) return false;
+    return true;
+  }
 }
 
 module.exports = HeightBalancedBST;
--- test/296_heightBalancedBSTTest.js
+++ test/296_heightBalancedBSTTest.js
@@ -1,16 +1,36 @@
 const assert = require('assert');
 const HeightBalancedBST = require('../daily/296_heightBalancedBST.js');
+const { Node } = require('../daily/3_serializeTree.js');
 
 let algo = null;
 
 beforeEach('Setting up...', function() {
-  //algo = new HeightBalancedBST();
+  algo = new HeightBalancedBST();
 });
-describe('Template', function() {
-  const expected = 3;
 
-  it('Iterative', function() {
-    const result = 3;//algo.iterative(WM)
-    assert.equal(result, expected);
+describe.only('HeightBalancedBST - 5 nodes', function() {
+  it('Recursive', function() {
+    const WM = ['1', '2', '3', '4', '5'];
+    const result = algo.recursive(WM);
+    assert.equal(algo.isBalanced(WM), true);
+    console.log(result);
+  });
+});
+
+describe.only('HeightBalancedBST - 7 nodes', function() {
+  it('Recursive', function() {
+    const WM = ['1', '2', '3', '4', '5', '6', '7'];
+    const result = algo.recursive(WM);
+    assert.equal(algo.isBalanced(WM), true);
+    console.log(result);
+  });
+});
+
+describe.only('HeightBalancedBST - 12 nodes', function() {
+  it('Recursive', function() {
+    const WM = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+    const result = algo.recursive(WM);
+    assert.equal(algo.isBalanced(WM), true);
+    console.log(result);
   });
 });
