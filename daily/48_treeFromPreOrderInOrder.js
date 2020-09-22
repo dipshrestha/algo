96b37b8adeaf186d349c6116727e3077a2d52588
dipesh <dipshrestha@gmail.com>
2020-09-21 21:10:05 -0700

    recurisve

--- daily/48_treeFromPreOrderInOrder.js
+++ daily/48_treeFromPreOrderInOrder.js
@@ -28,12 +28,95 @@ const { Node } = require('../daily/3_serializeTree.js');
 
 /*
  Algo: 
+ 	Find the first node
+ 		Build left/right tree
 */
-class TreeFromPreOrderInOrder{
+class TreeFromPreOrderInOrder {
+  recursive(preorder, inorder) {
+    this.V = new Array(inorder.length);
+    this.V.fill(0);
+    var res = this._recursive_helper(preorder, inorder);
+    console.log(res);
+    return res;
+  }
 
-  // Time: O(2^N)
-  // Space: O(N)
+  // the first preorder that is not -1 and is in inorder
+  _getRootIndex(preorder, inorder) {
+    var idx = -1,
+      elm;
+      debugger;
+    for (var i = 0; i < preorder.length; i++) {
+      if (this.V[i] == -1) continue;
+      elm = preorder[i];
+      idx = inorder.indexOf(elm);
+      if (idx < 0) continue;
+      break;
+    }
+    this.V[i] = -1;
+    return idx;
+  }
 
+  _recursive_helper(preorder, inorder) {
+    if (!inorder || inorder.length < 1) return null;
+    const r = this._getRootIndex(preorder, inorder);
+
+    const left = this._recursive_helper(preorder, inorder.slice(0, r));
+    const right = this._recursive_helper(preorder, inorder.slice(r + 1, inorder.length));
+    const node = new Node(inorder[r], left, right);
+    return node;
+  }
+  /*
+    // Time: O(2^N)
+    // Space: O(N)
+    recursive(preorder, inorder) {
+    	this.V = new Array(inorder.length);
+      var res = this._recursive_helper(0, preorder, inorder);
+      return res;
+    }
+
+    _getRoot(preorder) {
+      return preorder[0];
+    }
+
+    _getRootIndices(index, preorder, inorder) {
+      var elm = preorder[index];
+      return [preorder[index], inorder.indexOf(elm)];
+    }
+
+    _isLeftOfNode(leftElm, elm, arr) {
+      return arr.indexOf(leftElm) < arr.indexOf(elm);
+    }
+
+    _recursive_helper(index, preorder, inorder) {
+      debugger;
+      if (index >= inorder.length) return null;
+      // find left & right
+      var elm = preorder[index];
+      if (elm == null || null === this.V[index]) return null;
+      //this.V[index] = null;
+
+      var left = this._recursive_helper(index + 1, preorder, inorder);
+      var right = null;
+
+      if (!this._isLeftOfNode(left, elm, inorder)) {
+        right = left;
+        left = null;
+      } else {
+        var rightElmInInOrder = inorder[inorder.indexOf(elm) + 1];
+        if (rightElmInInOrder) {
+          console.log(rightElmInInOrder);
+          right = this._recursive_helper(preorder.indexOf(rightElmInInOrder), preorder, inorder);
+        }
+      }
+
+      var node = new Node(elm, left, right);
+      //preorder[index] = null;
+      this.V[index] = null;
+      console.log(node);
+      return node;
+
+    }
+  */
 }
 
 module.exports = TreeFromPreOrderInOrder;
--- test/48_treeFromPreOrderInOrderTest.js
+++ test/48_treeFromPreOrderInOrderTest.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const TreeFromPreOrderInOrder = require('../daily/48_treeFromPreOrderInOrder.js');
+const { Node } = require('../daily/3_serializeTree.js');
 
 let algo = null;
 
@@ -7,13 +8,24 @@ beforeEach('Setting up...', function() {
   algo = new TreeFromPreOrderInOrder();
 });
 
-describe('TreeFromPreOrderInOrder', function() {
-  const expected = 3;
-  const preT = ['a', 'b', 'd', 'e', 'c', 'f', 'g'];
-  const inT = ['d', 'b', 'e', 'a', 'f', 'c', 'g'];
+describe.only('TreeFromPreOrderInOrder', function() {
 
-  it('Iterative', function() {
-    const result = 3//algo.iterative(WM)
-    assert.equal(result, expected);
+  it('Recursive-complete tree, height 3', function() {
+    const preT = ['a', 'b', 'd', 'e', 'c', 'f', 'g'];
+    const inT = ['d', 'b', 'e', 'a', 'f', 'c', 'g'];
+    const expected = new Node('a', new Node('b', new Node('d'), new Node('e')),
+      new Node('c', new Node('f'), new Node('g')));
+    const result = algo.recursive(preT, inT)
+    assert.deepEqual(result, expected);
+  });
+
+
+  it('Recursive-complete tree, height 3', function() {
+    const preT = [5, 4, 2, 1, 6, 7, 9, 10]
+    const inT = [1, 2, 4, 6, 5, 7, 10, 9];
+    const expected = new Node(5, new Node(4, new Node(2, new Node(1)), new Node(6)),
+      new Node(7, null, new Node(9, new Node(10))));
+    const result = algo.recursive(preT, inT)
+    assert.deepEqual(result, expected);
   });
 });
